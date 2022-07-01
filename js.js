97acd34296d52b0639d22e61f8e4f21b9b86b199
hyunta12(강주훈) <93520535+rkdwngns@users.noreply.github.com>
2022-07-01 11:18:16 +0900

    Add files via upload

---
+++ js.js
@@ -0,0 +1,15 @@
+
+start();
+var imgs=3;
+var now =0;
+
+function start(){
+    $(".imgs>img").eq(0).siblings().css({"margin-left":"-2000px"});    
+    setInterval(function(){slide();}, 2000);
+}
+
+function slide(){
+    now = now == imgs?0:now+=1;
+    $(".imgs>img").eq(now-1).css({"margin-left":"-2000px"});
+    $(".imgs>img").eq(now).css({"margin-left":"0px"});
+}
